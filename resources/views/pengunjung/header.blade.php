619644475952e33f9e0bcd8fc3502899d4a94f65
Bernardinus Hendra Natadiria <me@hendranata.com>
2020-10-22 18:43:37 +0700

    Split header to separate file

---
+++ resources/views/pengunjung/header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="bootstrap dist/css/bootstrap.min.css">
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="jquery.min.js"></script>
+  <script src="popper.min.js"></script>
+  <script src="bootstrap dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Peluang Berbicara</title>
+</head>
+<body class="mt-5">
+
+  <!-- Navbar -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-warning">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">RuangBicara</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" href="home.html">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="detailpost.html">Postingan</a>
+          <a class="nav-link" href="profile.html">Profil</a>
+          <a class="nav-link" href="galeri.html">Galeri</a>
+          <a class="nav-link" href="contact.html">Contact</a>
+        </div>
+      </div>
+    </div>
+  </nav>
