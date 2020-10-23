89cbba6a08f695786d70abd0b741fa333bb39226
fionamulyono <fionamulyono@gmail.com>
2020-10-23 09:11:45 +0700

    ini index, header, footer

---
+++ resources/views/pengunjung/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer class="bg-warning text-black pt-3">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <p>PeluangBerbicara2020.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+  </html>
