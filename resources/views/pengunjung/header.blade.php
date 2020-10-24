f492773ae17dd7592c92ef6dd8697ef2ed4e6b8d
Meow-beep <cincauenak07@gmail.com>
2020-10-25 00:36:41 +0700

    please be smart ra

--- resources/views/pengunjung/header.blade.php
+++ resources/views/pengunjung/header.blade.php
@@ -25,26 +25,6 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-
-          <li class="nav-item">
-            <a class="nav-link" href="index.blade.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="singlepost.blade.php">Postingan</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0 ">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-dark my-2 my-sm-0 mr" type="submit">Search</button>
-        </form>
-        <ul class="navbar-nav">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              raraariefftah
-
           <li class="nav-item {{ request()->segment(1)==''?'active':''}} {{ request()->segment(1)=='home'?'active':''}}">
             <a class="nav-link" href="/">Home</a>
           </li>
